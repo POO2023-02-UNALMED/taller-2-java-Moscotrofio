f933d686e22a68c1d978dd5654c3f41e5ac6c63b
Moscotrofio <indarttomas@gmail.com>
2023-08-22 14:12:19 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,14 +1,14 @@
 package test;
 
 public class Auto {
-    String modelo;
-    int precio;
-    Asiento[] asientos;
-    String marca;
-    Motor motor;
-    int registro;
-    public static int cantidadCreados = 0;
-
+	String modelo;
+	int precio;
+	Asiento [] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
 	int cantidadAsientos() {
 		int numAsientos=0;
 		for (int z=0; z<asientos.length; z++) {
@@ -18,23 +18,23 @@ public class Auto {
 		}
 		return numAsientos;
 	}
-
-    String verificarIntegridad() {
-        boolean original = true;
-        if (this.registro != this.motor.registro) {
-        original = false;
-        } else {
-            for (Asiento j : this.asientos) {
-                if (j.registro != this.registro) {
-                    original = false;
-                    break;
-                }
-            }
-        }
-        if (original) {
-            return "Auto original";
-        } else {
-            return "Las piezas no son originales";
-        }
-    }
+	String verificarIntegridad() 
+	{
+		boolean verificar = true;
+		if (motor.registro!=registro) {
+			verificar = false;
+		}
+		for(int z=0; z<asientos.length; z++) {
+			if (asientos[z]!=null && asientos[z].registro!=registro) {
+				verificar = false;
+				break;
+			}
+		}
+		if (verificar==true) {
+			return "Auto original";
+		}
+		else {
+			return "Las piezas no son originales";
+		}
+	}
 }
